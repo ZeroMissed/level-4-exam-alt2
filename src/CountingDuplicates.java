6cba4b4382d1a7f62415fb47a36abaacec6581f2
RyanN <r.nagle64@gmail.com>
2017-05-27 11:45:29 -0700

    boom

--- src/CountingDuplicates.java
+++ src/CountingDuplicates.java
@@ -1,8 +1,27 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CountingDuplicates {
 	public static int duplicateCount(String word) {
-		return 0;
+		int numDouble = 0;
+		ArrayList<String> chars = new ArrayList<String>();
+		ArrayList<String> checked = new ArrayList<String>();
+		
+		for(char i : word.toCharArray()) {
+			chars.add(i + "");
+		}
+		for(int i = chars.size() - 1; i >= 0; i--) {
+			String cur = chars.get(i);
+			chars.remove(i);
+			if(!checked.contains(cur)) {
+				if(chars.contains(cur.toLowerCase()) || chars.contains(cur.toUpperCase()) ) {
+				numDouble++;
+				}
+			}
+			checked.add(cur.toLowerCase());
+			checked.add(cur.toUpperCase());
+		}
+		return numDouble;
 	}
 	
 	public static void main(String[] args) {
